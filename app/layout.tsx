a15c285d4926ed577b3da1ddfe2abd65b0af326f
PeeCee45 <ibehpromise45@gmail.com>
2026-02-01 07:10:08 +0100

    Corrected input zoom to the screen on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,48 @@ const onest = Onest({
   variable: "--font-onest",
 });
 
+import { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "Letmoveit.ng",
   description: "Move stress-free with LetmoveIt.ng, Nigeria’s top choice for reliable, eco-friendly moving services. Whether you're relocating your home or business, our advanced move estimate calculator and professional packing solutions ensure a seamless experience.",
+  keywords: ["Letmoveit", "letmoveit.ng", "Letmoveit ng", "Letmoveit Abuja", "Letmoveit Nigeria"],
+  applicationName: "Letmoveit.ng",
+  developer: [{ name: "PeeCee Devs", email: "ibehpromise45@gmail.com" }],
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
+  icons: {
+    icon: "https://www.letmoveit.ng/assets/images/logo.png",
+  },
+  openGraph: {
+    title: "Letmoveit.ng",
+    description: "Move stress-free with LetmoveIt.ng, Nigeria’s top choice for reliable, eco-friendly moving services. Whether you're relocating your home or business, our advanced move estimate calculator and professional packing solutions ensure a seamless experience.",
+    url: "https://www.letmoveit.ng",
+    siteName: "Letmoveit.ng",
+    type: "website",
+    images: [
+      {
+        url: "https://www.letmoveit.ng/assets/images/Group 375.png",
+        width: 107,
+        height: 107,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    title: "Letmoveit.ng",
+    description: "Move stress-free with LetmoveIt.ng, Nigeria’s top choice for reliable, eco-friendly moving services. Whether you're relocating your home or business, our advanced move estimate calculator and professional packing solutions ensure a seamless experience.",
+    images: ["https://www.letmoveit.ng/assets/images/Group 375.png"],
+    // creator: "@Letmoveitng",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://www.letmoveit.ng",
+  },
 };
+
 
 export default function RootLayout({
   children,
